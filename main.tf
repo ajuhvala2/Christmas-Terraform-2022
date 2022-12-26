044ad67deb5ad907437027492f219852ad4317fd
ajuhvala2 <ajuhvala2gmail.com>
2022-12-25 22:20:33 -0500

    Provider-credentials

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+  profile ="vala-terraform-user"
+}
+
+#stores my statefile in S3
+terraform {
+  backend "s3" {
+    bucket = "vala-sucess-bank-terrafron-statefile"
+    key    = "terraform.tfstate.dev"
+    region = "us-east-1"
+   #profile ="vala-terraform-user"
+  }
+}
+
+
+
